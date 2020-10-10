3f8a930080587d17161887252b16dc902745cdad
Nou Vang <nou.muas@gmail.com>
2020-10-10 10:33:17 -0500

    fix error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,9 +4,6 @@ const burger = require("../models/burger.js");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
-
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
